4766e8756d95e1faf7d16f4f895f9e5411980585
naa986 <naa986@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2013-04-01 02:52:13 +0000

    .committing first version of the plugin
    git-svn-id: https://plugins.svn.wordpress.org/easy-video-player/trunk@689768 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ easy-video-player.php
@@ -0,0 +1,129 @@
+<?php
+/*
+Plugin Name: Easy Video Player
+Version: 1.0.1
+Plugin URI: http://easywpguide.wordpress.com/?p=25
+Author: naa986
+Author URI: http://easywpguide.wordpress.com/
+Description: Easily embed videos into your WordPress blog
+*/
+
+if(!defined('ABSPATH')) exit;
+if(!class_exists('EASY_VIDEO_PLAYER'))
+{
+	class EASY_VIDEO_PLAYER 
+	{
+		var $plugin_version = '1.0.1';
+		function __construct() 
+		{
+			define('EASY_VIDEO_PLAYER_VERSION', $this->plugin_version);
+			$this->plugin_includes();
+		}
+		function plugin_includes()
+		{
+			if(is_admin( ) ) 
+			{
+				add_filter('plugin_action_links', array(&$this,'easy_video_player_plugin_action_links'), 10, 2 );
+			}
+                        add_action('wp_enqueue_scripts', 'easy_video_player_enqueue_scripts');
+			add_action('admin_menu', array( &$this, 'easy_video_player_add_options_menu' ));
+			add_shortcode('evp_embed_video','evp_embed_video_handler');	
+		}
+		function plugin_url() 
+		{
+			if($this->plugin_url) return $this->plugin_url;
+			return $this->plugin_url = plugins_url( basename( plugin_dir_path(__FILE__) ), basename( __FILE__ ) );
+		}
+		function easy_video_player_plugin_action_links($links, $file) 
+		{
+			if ( $file == plugin_basename( dirname( __FILE__ ) . '/easy-video-player.php' ) ) 
+			{
+				$links[] = '<a href="options-general.php?page=easy-video-player-settings">Settings</a>';
+			}
+			return $links;
+		}
+		
+		function easy_video_player_add_options_menu()
+		{
+			if(is_admin())
+			{
+				add_options_page('Easy Video Player Settings', 'Easy Video Player', 'manage_options', 'easy-video-player-settings', array(&$this, 'easy_video_player_options_page'));
+			}
+			add_action('admin_init', array(&$this, 'easy_video_player_add_settings'));		
+		}
+		function easy_video_player_add_settings()
+		{ 
+			register_setting('easy-video-player-settings-group', 'evp_enable_jquery');	
+		}
+		function easy_video_player_options_page()
+		{
+			
+			?>
+			<div class="wrap">
+			<div id="poststuff"><div id="post-body">
+			
+			<h2>Easy Video Player - v<?php echo $this->plugin_version;?></h2>
+                        <div class="postbox">
+			<h3><label for="title">Setup Guide</label></h3>
+			<div class="inside">		
+                            <p>For detailed documentation please visit the plugin homepage <a href="http://easywpguide.wordpress.com/?p=25" target="_blank">here</a></p>
+			</div></div>
+			<div class="postbox">
+			<h3><label for="title">General Settings</label></h3>
+			<div class="inside">		
+			<form method="post" action="options.php">
+			    <?php settings_fields('easy-video-player-settings-group'); ?>
+			    <table class="form-table">
+			        <tr valign="top">
+			        <th scope="row">Enable jQuery</th>
+			        <td><input type="checkbox" id="evp_enable_jquery" name="evp_enable_jquery" value="1" <?php echo checked(1, get_option('evp_enable_jquery'), false) ?> /> 
+			        <p><i>By default this option should always be checked.</i></p>
+			        </td>
+			        </tr>
+			    </table>
+			    
+			    <p class="submit">
+			    <input type="submit" class="button-primary" value="<?php _e('Save Changes') ?>" />
+			    </p>		
+			</form>
+			</div></div>
+			
+			</div></div>
+			</div>
+			<?php
+		}
+	}
+	$GLOBALS['easy_video_player'] = new EASY_VIDEO_PLAYER();
+}
+
+function easy_video_player_enqueue_scripts()
+{
+    if (!is_admin()) 
+    {
+        $plugin_url = plugins_url('',__FILE__);
+        $enable_jquery = get_option('evp_enable_jquery');
+        if($enable_jquery)
+        {
+            wp_enqueue_script('jquery');
+        }
+    	wp_register_script('flowplayer-js', $plugin_url.'/lib/flowplayer.js');
+	wp_enqueue_script('flowplayer-js');
+    	wp_register_style('flowplayer-css', $plugin_url.'/lib/minimalist.css');
+        wp_enqueue_style('flowplayer-css');	
+    }	
+}
+
+function evp_embed_video_handler($atts)
+{
+	extract(shortcode_atts(array(
+            'url' => '',
+	), $atts));
+	$output = <<<EOT
+        <div data-ratio="0.417" class="flowplayer">
+        <video>
+           <source type="video/mp4" src="$url"/>
+        </video>
+        </div>
+EOT;
+	return $output; 
+}
---
+++ readme.txt
@@ -0,0 +1,65 @@
+=== Easy Video Player ===
+Contributors: naa986
+Donate link: http://easywpguide.wordpress.com/
+Tags: video, wpvideo, flash, html5, iPad, iphone, ipod, mobile, playlists, embed video, flowplayer, video html5, flash player, player, video player
+Requires at least: 3.0.1
+Tested up to: 3.5.1
+Stable tag: 1.0.1
+License: GPLv3 or later
+License URI: http://www.gnu.org/licenses/gpl-3.0.html
+
+Easy Video Player allows you to embed videos into your WordPress site.
+
+== Description ==
+
+Easy Video Player is a user-friendly WordPress plugin to showcase your videos. You can embed both self-hosted videos or videos that are external hosted using direct links.
+
+= Easy Video Player Features =
+
+* Embed MP4 videos into your blog.
+* Embed responsive videos for a better user experience while viewing from a mobile device.
+* Embed HTML5 videos which are compatible with all major browsers.
+
+= Easy Video Player Plugin Usage =
+
+*Settings Configuration*
+
+It's pretty easy to set up this plugin. Once you have installed the plugin simply navigate to the Settings menu where you will be able to configure some options. Mostly you just to need check the "Enable jQuery" option. That will allow the plugin to make use of jQuery library.
+
+*Embedding Shortcodes for the Videos*
+
+Now it's time to finally embed a video shortcode. To do this create a new post/page and use the following shortcode:
+
+`[evp_embed_video url="http://example.com/wp-content/uploads/videos/myvid.mp4"]`
+
+here, url is a shortcode parameter that you need to replace the actual URL of the video file.
+
+For detailed documentation please visit the [Easy Video Player](http://easywpguide.wordpress.com/?p=25) plugin page
+
+
+== Installation ==
+
+1. Go to the Add New plugins screen in your WordPress Dashboard
+1. Click the upload tab
+1. Browse for the plugin file (easy-video-player.zip) on your computer
+1. Click "Install Now" and then hit the activate button
+1. Now, go to the settings menu of the plugin and follow the instructions for embedding videos.
+
+== Frequently Asked Questions ==
+
+= Can this plugin be used to embed videos on my WordPress blog? =
+
+Yes.
+
+= Are the videos embedded by this plugin playable on iOS devices? =
+
+Yes.
+
+== Screenshots ==
+
+For screenshots please visit the [Easy Video Player](http://easywpguide.wordpress.com/?p=25) plugin page
+
+== Changelog ==
+
+= 1.0.1 =
+* First commit
